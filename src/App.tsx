f28f758b69c710cbd858184d97141543f3435c0d
Sejan~ <md4sejan@gmail.com>
2024-06-27 22:16:37 +0600

    prject setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <h2>My ToDo</h2>
+      <Button>Click</Button>
+    </div>
+  );
+};
+
+export default App;
